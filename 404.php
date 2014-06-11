96f8aa1993a60b3c844e92871b26b782fd20236c
zenynunez <info@zenycr.com>
2014-06-11 16:52:56 -0600

    test

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
 		<div class="row-fluid">
 			<div class="span12">
 				<h1 class="largest">404</h1>
-				<h1><?php _e('Oops! Page not found', ETHEME_DOMAIN) ?></h1>
+				<h1><?php _e('Oops! Pagina no encontrada', ETHEME_DOMAIN) ?></h1>
 				<p><?php _e('Sorry, but the page you are looking for is not found. Please, make sure you have typed the current URL.', ETHEME_DOMAIN) ?> </p>
 				<a href="<?php echo home_url(); ?>" class="button medium"><?php _e('Go to home page', ETHEME_DOMAIN); ?></a>
 			</div>
